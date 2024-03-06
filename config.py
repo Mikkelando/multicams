bc54d6bc649680f97ecefd75f9ab51284b1b339c
Mikkelando <ms.m.kl@mail.ru>
2024-03-06 18:54:26 +0300

    post init ops

---
+++ config.py
@@ -0,0 +1,5 @@
+conf = {
+    'Version': '1.0.0',
+    'Name' : 'multicams',
+    'Author' : 'RED'
+}
--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 if __name__ == "__main__":
     print('run')
+    from config import conf
+    print(conf)
